819d5dbe2e2bdf91197fd43bc835ada7cebbda70
AriiSib <foxirt@mail.ru>
2022-10-28 19:54:26 +0600

    Revert "Added a wellcome line"
    This reverts commit eb6da86a7cd024af1ba91a9e73fb6b1ed9975dd9.

--- Main.java
+++ Main.java
@@ -5,10 +5,8 @@ public class Main {
 
     public static void main(String[] args) {
         Algorithm algorithm;
-		
-		System.out.println("Hello user!")
+
         System.out.println("\nЗадание 8: Вывести числа Капрекара в указанном диапазоне. \n");
-		
 
         do {
             algorithm = new Algorithm();
